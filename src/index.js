9b69d16b8608c1b2f258cd882383454858f8c409
Zane Jakuseva <zane.knn@gmail.com>
2023-12-05 08:22:42 +0100

    Updated api key

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ function search(event) {
   let searchInputElement = document.querySelector("#search-input");
   let city = searchInputElement.value;
 
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiKey = "bta8d0cd5f43b6fe4f1cd0019401obcd1";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayTemperature);
